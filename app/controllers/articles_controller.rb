e938d5fafa09ab34c7508f27e8ee9cca417a39d1
Daniel Perez <daniel.perez@customink.com>
2022-06-14 14:05:35 -0400

    ADM

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,57 @@
+class ArticlesController < ApplicationController
+  # GET
+  def index
+    @articles = Article.all
+  end
+
+  # GET
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  # GET
+  def new
+    @article = Article.new
+  end
+
+  # POST
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # GET
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  # PATCH
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+    # Strong params
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+end
